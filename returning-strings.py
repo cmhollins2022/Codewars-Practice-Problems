63c6d66489e9ba930d9b0adf09628df04d33b9ab
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-04-27 21:55:13 -0400

    Update returning-strings.py

--- returning-strings.py
+++ returning-strings.py
@@ -1,5 +1,5 @@
 def greet(name: str):
-    return print(f"Hello, {name} how are you doing today")
+    return print(f"Hello, {name} how are you doing today") # Return an f-string that includes the variable passed.
 
 
 greet("Name")  # create a function that returns a string with someone's name in it.
