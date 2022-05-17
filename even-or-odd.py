b7bc65ab2d69dc2cee97381677362af51b75c76a
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-05-16 22:36:16 -0400

    Create even-or-odd.py

---
+++ even-or-odd.py
@@ -0,0 +1,5 @@
+def even_or_odd(number):
+    if number % 2 == 0:  # check if number can be divided evenly by 2
+        return print("Even")  # if so, print "even"
+    else:
+        return print("Odd")  # if not, print "odd"
