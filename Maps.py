20e5f1c1dfaf07df33eb22e1af3704dc5712a1f9
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-05-19 15:48:14 -0400

    Create Maps.py

---
+++ Maps.py
@@ -0,0 +1,6 @@
+def maps(a):
+    new_list = []  # make a new list to store the values to be added
+    for x in a:  # iterate
+        doubled_x = x * 2  # double x
+        new_list.append(doubled_x)  # add doubled x to new list
+    return print(new_list)
