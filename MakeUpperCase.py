147501c54a57715271b4a9e395454268355d02fc
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-05-19 15:58:18 -0400

    Update MakeUpperCase.py

--- MakeUpperCase.py
+++ MakeUpperCase.py
@@ -2,3 +2,9 @@ def make_upper_case(s: str):
     return print(s.upper())  # Make uppercase
 
 # Alternatively...
+
+def make_upper_case(s: str):
+    if type(s) != str: # Will try to use another built in method for number strings.
+        return print("Incorrect data type. Please try again.")
+        
+    return print(s.upper())  # Make uppercase
