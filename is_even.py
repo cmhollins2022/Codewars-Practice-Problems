7ca7e11ca38f02032dc01bfed461465b1c877e8d
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-05-21 22:30:25 -0400

    Create is_even.py

---
+++ is_even.py
@@ -0,0 +1,9 @@
+# Your code will determine if the number passed is even (or not).
+# The function needs to return either a true or false.
+# Numbers may be positive or negative, integers or floats.
+
+def is_even(n: int): 
+    if n % 2 == 0: 
+        return True
+    else: # Anything else would be false
+        return False
