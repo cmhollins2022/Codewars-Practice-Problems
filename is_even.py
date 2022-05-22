42573425b3de084a51c643ed44b1bb90a4858373
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-05-21 22:31:02 -0400

    Update is_even.py

--- is_even.py
+++ is_even.py
@@ -3,7 +3,7 @@
 # Numbers may be positive or negative, integers or floats.
 
 def is_even(n: int): 
-    if n % 2 == 0: 
+    if n % 2 == 0: # Use Modulo in order to determine if the number is divisible by 2
         return True
     else: # Anything else would be false
         return False
