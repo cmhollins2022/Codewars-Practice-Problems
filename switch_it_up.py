fa9f55f02350f3a9962dd762992407995702fffb
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-06-03 15:12:17 -0400

    Create switch_it_up.py

---
+++ switch_it_up.py
@@ -0,0 +1,8 @@
+def switch_it_up(number: int): # Specify our type.
+    numbers_dict = { # Create a dictionary for all of the key-value pairs.
+        0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four",
+        5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+    }
+    return numbers_dict.get(number) # Index our function input from the dictionary.
+  
+  # Another way...
