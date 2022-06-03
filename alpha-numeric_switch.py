9ef7068df517a23682fc197af01dd66ccdbb8a06
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-06-03 14:59:40 -0400

    Update alpha-numeric_switch.py

--- alpha-numeric_switch.py
+++ alpha-numeric_switch.py
@@ -1,4 +1,4 @@
-def correct(word: str):
+def correct(word: str): # Verify the function input type.
     word = word.replace("0", "O") # Set our data of word equal to the revision.
     word = word.replace("1", "I")
     word = word.replace("5", "S")# Verify our data type.
