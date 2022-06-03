81863c08524970bda5de42b2af90e73ab37c001e
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-06-02 22:02:38 -0400

    Update vowel_count.py

--- vowel_count.py
+++ vowel_count.py
@@ -2,5 +2,7 @@
 def get_count(sentence: str): # Type our inputing method
     vowels_list = ["a","e","i","o","u"] # Make a vowel list
     count = 0 # Be prepared to count the vowels
-    
+    for letter in sentence: # Iterate through our string
+        if letter in vowels_list: # If the letter is a vowel...
+            count = count + 1 # Increase our count by +1
     return count
