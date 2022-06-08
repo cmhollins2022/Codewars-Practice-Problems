168e8b20cba7ee8b8d7b049af2f7ba2051ad4bb5
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-06-07 20:20:52 -0400

    Update string_ends_with.py

--- string_ends_with.py
+++ string_ends_with.py
@@ -1,3 +1,3 @@
 def solution(text: str, text_ending: str):  # Type the data types
-    if text.endswith(text_ending):  
-
+    if text.endswith(text_ending):  # Use the ".endswith()" method
+       
