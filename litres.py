d49e0127a1e746eb8517ac4b319ac6f447979524
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-08-05 19:07:17 -0400

    Update litres.py

--- litres.py
+++ litres.py
@@ -4,3 +4,11 @@ def litres(time: float):
     return math.floor(time/2)
   
   # Good practice using a method from the "math" library.
+
+# Alternate ways...
+
+def litres(time):
+    return int(time/2)
+
+def litres(time):
+    return int(time/2)
