9e271545869c43803dc9af52e5ec6bde0e475390
C. Maxwell Hollins <101115970+cmhollins2022@users.noreply.github.com>
2022-08-05 16:37:01 -0400

    Create Banjo.py

---
+++ Banjo.py
@@ -0,0 +1,16 @@
+# First way...
+# def are_you_playing_banjo(name: str):
+#     if name.capitalize()[0] == "R":
+#         return f"{name} plays banjo"
+#     else:
+#         return f"{name} does not play banjo"
+    
+def are_you_playing_banjo(name: str):
+    if name[0] == "r" or name[0] == "R":
+        return f"{name} plays banjo"
+    else:
+        return f"{name} does not play banjo"
+      
+# Alternatively... 
+def areYouPlayingBanjo(name):
+    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo";
